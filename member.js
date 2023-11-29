d288e058760438cba2bdec86b9df93d8fe801b3b
eduardoquintero117 <31216906+eduardoquintero117@users.noreply.github.com>
2023-11-29 22:36:13 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,12 @@
+function skillsMember(){
+
+    var member = {
+        name: 'John',
+        age: 25,
+        skills: ['js', 'html', 'css'],
+        salary: 2000,
+        bonus: 200
+    };
+
+    return member;
+}
